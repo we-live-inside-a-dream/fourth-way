47149691e4837680056f46e2031f01125a818abf
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2025-12-26 13:38:34 -0700

    fix: Warn about unset VITE_API_URL in production builds.

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+
+if (import.meta.env.PROD && !import.meta.env.VITE_API_URL) {
+    console.warn("⚠️ VITE_API_URL is not set! App is trying to connect to localhost:8080 which will fail on a live site.");
+}
